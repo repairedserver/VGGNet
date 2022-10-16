94aa77c821cbb6f44fa81796cdb183554f51f997
김세현 <dnjfdid14@naver.com>
2022-10-16 21:16:15 +0900

    모델

--- dataloader.py
+++ dataloader.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import torch.nn as nn
 import torchvision.transforms as transforms
 
 #transform과 데이터셋
@@ -15,3 +16,51 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
 
 testset = torchvision.datasets.STL10(root='./data', split='test', download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+
+# VGG_A 모델링
+class vgg_a(nn.Module): # 깊이 11층
+    def __init__(self, num_classes: int = 1000, init_weights: bool = True):
+        super(vgg_a, self).__init__()
+        self.convnet = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 224->112
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 112->56
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 56->28
+
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 28->14
+
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)  # 14->7
+        )
+
+        self.fclayer = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.convnet(x)
+        x = torch.flatten(x, 1)
+        x = self.fclayer(x)
+        return x
