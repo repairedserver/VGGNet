0a12775a804781c01812470ccb44b854fadce53c
김세현 <dnjfdid14@naver.com>
2022-10-16 20:55:18 +0900

    학습파일

--- train.py
+++ train.py
@@ -1,1 +1,17 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
+
+#transform과 데이터셋
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.RandomCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+trainset = torchvision.datasets.STL10(root='./data', split='train', download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+
+testset = torchvision.datasets.STL10(root='./data', split='test', download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
