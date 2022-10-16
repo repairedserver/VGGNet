a8d5d5ee6b30eac3d3a9e1a3e930b5102c77e1ed
김세현 <dnjfdid14@naver.com>
2022-10-16 21:25:42 +0900

    모델

--- model.py
+++ model.py
@@ -11,9 +11,19 @@ vis.close(env="main")
 
 # 손실 추적기
 def loss_tracker(loss_plot, loss_value, num):
-    '''num, loss_value, are Tensor'''
     vis.line(X=num,
              Y=loss_value,
              win=loss_plot,
              update='append')
 
+# gpu가 있으면 gpu 사용, 없으면 cpu 사용
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+torch.manual_seed(777)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
